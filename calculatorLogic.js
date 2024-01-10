67b692865d96b460e02208f3ade434f4b5ac4055
Ling <126978057+chenlingj999@users.noreply.github.com>
2024-01-09 20:04:31 -0500

    Implement operate function which chooses proper operation

--- calculatorLogic.js
+++ calculatorLogic.js
@@ -13,3 +13,30 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+let num1;
+let num2;
+let operator;
+
+function operate(num1, operator, num2) {
+    switch(operator) {
+        case "+":
+            return add(num1, num2);
+            break;
+
+        case "-":
+            return subtract(num1, num2);
+            break;
+
+        case "*":
+            return multiply(num1, num2);
+            break;
+
+        case "/":
+            return divide(num1, num2);
+            break;
+        
+        default:
+            alert("Unsupported Operator!")
+    }
+}
